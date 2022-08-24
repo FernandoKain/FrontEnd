4e432108d572973ec961ec5cd8e7f8ad883bf605
FernandoKain <fernandomanga@hotmail.com>
2022-08-24 08:17:13 -0300

    Como utilizar e importar módulos em javascript

---
+++ Modulos-Javascript/main.mjs
@@ -0,0 +1,5 @@
+import {mostraCidade, mostraHobby, mostraIdade} from "./funcoes.mjs";
+
+console.log(mostraIdade("Fernando", 42));
+console.log(mostraHobby('Fernando', 'Codar'));
+console.log(mostraCidade('Fernando', 'Sorocaba'));
